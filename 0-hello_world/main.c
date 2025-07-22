bad9a025f5bb8f9558f1afab7567d6c11a2b129e
Caio Vieira <caioviier@gmail.com>
2025-07-22 19:42:36 -0300

    initial

---
+++ 0-hello_world/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  
+  // this is a comment
+  // this does not affect the script
+  // is just information 
+
+  /*
+    this is also a comment
+    but used for many lines
+
+    below, 'printf' shows the text
+    between quotes in the terminal
+
+    the return 0 is used to say
+    'this code executed successfully'
+    :)
+
+    return 1, for example, means that
+    it had an error :/
+  */
+
+  printf("Hello World!");
+
+  return 0;
+}
---
+++ 1-variables/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  
+  /*
+    we have many types of variables
+    variables are representation for values
+    variables can be:
+    - int (integer numbers)
+    - float (decimal numbers)
+    - char (characters)
+    - bool (boolean, true or false)
+    and many others :)
+  */
+
+  int age = 25;
+
+  /*
+    to print a variable, we use the
+    %x structure
+    % says it is an structure
+    x says its type
+    
+  */
+
+  printf("I am %d years old", age);
+
+  return 0;
+}
